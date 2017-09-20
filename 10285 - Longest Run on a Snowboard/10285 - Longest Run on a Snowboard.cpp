f42a4b8abd1b4c9474e081a58ac7c692cb808d76
gcli <star106121@gmail.com>
2017-09-20 11:36:24 +0800

    Modify 10285.

--- 10285 - Longest Run on a Snowboard/10285 - Longest Run on a Snowboard.cpp
+++ 10285 - Longest Run on a Snowboard/10285 - Longest Run on a Snowboard.cpp
@@ -1,74 +1,53 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
+int row, column, longest;
 int base[105][105];
-int longestPath, rows, columns;
+bool used[105][105];
 
-void trace(int x, int y, int pathCounter) {	
-	if(x > 0 && base[x-1][y] != -1 && base[x-1][y] < base[x][y]) {
-		int temp = base[x][y];
-		base[x][y] = -1;
-		pathCounter++;
-		if(pathCounter > longestPath)
-			longestPath = pathCounter;
-		trace(x - 1, y, pathCounter);
-		pathCounter--;
-		base[x][y] = temp;
-	}
-	if(x < rows - 1 && base[x+1][y] != -1 && base[x+1][y] < base[x][y]) {
-		int temp = base[x][y];
-		base[x][y] = -1;
-		pathCounter++;
-		if(pathCounter > longestPath)
-			longestPath = pathCounter;
-		trace(x + 1, y, pathCounter);
-		pathCounter--;
-		base[x][y] = temp;
-	}
-	if(y > 0 && base[x][y-1] != -1 && base[x][y-1] < base[x][y]) {
-		int temp = base[x][y];
-		base[x][y] = -1;
-		pathCounter++;
-		if(pathCounter > longestPath)
-			longestPath = pathCounter;
-		trace(x, y - 1, pathCounter);
-		pathCounter--;
-		base[x][y] = temp;
-	}
-	if(y < columns - 1  && base[x][y+1] != -1 && base[x][y+1] < base[x][y]) {
-		int temp = base[x][y];
-		base[x][y] = -1;
-		pathCounter++;
-		if(pathCounter > longestPath)
-			longestPath = pathCounter;
-		trace(x, y + 1, pathCounter);
-		pathCounter--;
-		base[x][y] = temp;
-	}
+void travel(int x, int y, int counter) {
+	if(counter > longest)
+		longest = counter;
+
+	if(used[x][y])
+		return;
+
+	used[x][y] = true;
+	if(x - 1 >= 0 && base[x-1][y] < base[x][y])
+		travel(x - 1, y, counter + 1);
+	if(x + 1 < row && base[x+1][y] < base[x][y])
+		travel(x + 1, y, counter + 1);
+	if(y - 1 >= 0 && base[x][y-1] < base[x][y])
+		travel(x, y - 1, counter + 1);
+	if(y + 1 < column && base[x][y+1] < base[x][y])
+		travel(x, y + 1, counter + 1);
+	used[x][y] = false;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
 	int cases = 0;
-	
+
 	cin >> cases;
 	while(cases--) {
 		string name = "";
 		
-		cin >> name >> rows >> columns;
-		
-		memset(base, -1, sizeof(base));
-		for(int i = 0; i < rows; i++)
-			for(int j = 0; j < columns; j++)
+		cin >> name >> row >> column;
+		for(int i = 0; i < row; i++)
+			for(int j = 0; j < column; j++) {
 				cin >> base[i][j];
-				
-		longestPath = 0;
-		for(int i = 0; i < rows; i++)
-			for(int j = 0; j < columns; j++)
-				trace(i, j, 1);
-				
-		cout << name << ": " << longestPath << endl;
+				used[i][j] = false;
+			}
+
+		longest = 0;
+		for(int i = 0; i < row; i++)
+			for(int j = 0; j < column; j++)
+				travel(i , j, 1);
+
+		cout << name << ": " << longest << "\n";
 	}
-	
+
 	return 0;
 }
